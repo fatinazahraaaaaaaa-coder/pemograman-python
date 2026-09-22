0a443177e44e97b99db7f5fb05418b1666eadbc2
fatinazahraaaaaaaa-coder <fatinazahraaaaaaaa@gmail.com>
2026-09-22 17:51:17 +0700

    Add files via upload

---
+++ keywordwhile.py
@@ -0,0 +1,10 @@
+should_contineu = True
+
+while should_contineu:
+    n = int(input("enter an even number greater than 0: "))
+
+    if n <= 0 or n % 2 == 1:
+        print(n, "is not an even number greater than 0")
+        should_contineu = False
+    else:
+        print("number:", n)
---
+++ komentar.py
@@ -0,0 +1,5 @@
+# ini adalah komentar
+print("hallo,")
+print("selamat pagi!") # ini juga komentar
+
+# print ln("statement ini tidak akan dipanggil")
